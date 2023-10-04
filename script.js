cec977e74bb956658882ce4ec5e224deffddd4a8
Yusuke Toyama <131319857+koko1928@users.noreply.github.com>
2023-10-04 12:28:16 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function showSection(sectionId) {
-    // Hide all sections
     let sections = document.getElementsByTagName("section");
     for (let i = 0; i < sections.length; i++) {
         sections[i].style.display = "none";
